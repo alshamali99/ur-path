2619ed91277e06343cf5ecad320f7e1af23e11de
alshamali99 <alshamali.99@gmail.com>
2025-06-17 03:40:04 +0300

    Delete dash directory

--- dash/nav/user_in_header.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SESSION['num'] && $_SESSION['num'] != 0 && $_SESSION['password']) {
-    $u_id_in_header = intval($_SESSION['num']);
-    $u_query_in_header = mysqli_query($conn, "SELECT * FROM users WHERE id = $u_id_in_header") or die("U_Func_Err");
-    $user_l = mysqli_fetch_array($u_query_in_header);
-}
-?>
